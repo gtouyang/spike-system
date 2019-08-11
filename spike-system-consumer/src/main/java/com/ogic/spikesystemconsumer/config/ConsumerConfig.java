ca69ae60ac47dcb33bb6985f0c7c2c32a1004f86
ogic <ogic@Mint.com>
2019-08-11 22:01:22 +0800

    完成show_goods.html以及login.html部分在静态资源上的前后端对接

--- spike-system-consumer/src/main/java/com/ogic/spikesystemconsumer/config/ConsumerConfig.java
+++ spike-system-consumer/src/main/java/com/ogic/spikesystemconsumer/config/ConsumerConfig.java
@@ -1,17 +1,11 @@
 package com.ogic.spikesystemconsumer.config;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
+/**
+ * @author ogic
+ */
 @Configuration
-
 public class ConsumerConfig {
 
-    @Bean
-    @LoadBalanced
-    public RestTemplate initRestTemplate(){
-        return new RestTemplate();
-    }
 }
